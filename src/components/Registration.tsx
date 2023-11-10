d291ba2b20164597b33767b538bb8cfa7fb73ec5
Manolache Gabriela <manolachegabriela23@gmail.com>
2023-11-11 01:02:11 +0200

    added login and registration functionality

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,102 +1,136 @@
-import { ReactNode } from 'react';
-
+// import { ReactNode } from 'react';
+import { ChangeEvent, useState } from 'react';
 export default function Registration() {
-	const day: ReactNode[] = [];
-	const month: ReactNode[] = [];
-	const year: ReactNode[] = [];
-	for (let i = 1; i <= 31; i++) {
-		day.push(<option value={`${i}`}>{i}</option>);
-	}
-	for (let i = 1; i <= 12; i++) {
-		month.push(<option value={`${i}`}>{i}</option>);
-	}
-	for (let i = 2023; i >= 1990; i--) {
-		year.push(<option value={`${i}`}>{i}</option>);
-	}
+  interface RegistrationInput {
+    label: string;
+    htmlFor: string;
+    id: string;
+    name: string;
+    type: string;
+    autocomplete: string | undefined;
+    required: boolean;
+    onInput: (value: ChangeEvent<HTMLInputElement>) => void;
+  }
 
-	return (
-		<div className='w-full flex flex-col justify-center gap-8 items-center py-10 min-h-[70vh] font-poppins'>
-			<div>
-				<h3 className='text-xl lg:text-2xl text-secondary font-semibold'>
-					Create your Free Account
-				</h3>
-			</div>
-			<form
-				className='space-y-6 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4'
-				action='#'
-				method='POST'
-			>
-				<div>
-					<label
-						htmlFor='name'
-						className='block text-sm font-semibold leading-6 text-primary'
-					>
-						Name
-					</label>
-					<div className='mt-2'>
-						<input
-							id='name'
-							name='name'
-							type='name'
-							autoComplete='name'
-							required
-							className='px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6'
-						/>
-					</div>
-				</div>
-				<div>
-					<label
-						htmlFor='email'
-						className='block font-semibold text-sm leading-6 text-primary'
-					>
-						Email address
-					</label>
-					<div className='mt-2'>
-						<input
-							id='email'
-							name='email'
-							type='email'
-							autoComplete='email'
-							required
-							className='px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6'
-						/>
-					</div>
-				</div>
-				<div>
-					<label
-						htmlFor='password'
-						className='block font-semibold text-sm leading-6 text-primary'
-					>
-						Password
-					</label>
-					<div className='mt-2'>
-						<input
-							id='password'
-							name='password'
-							type='password'
-							required
-							className='px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6'
-						/>
-					</div>
-				</div>
-				<div>
-					<label
-						htmlFor='re-password'
-						className='block font-semibold text-sm leading-6 text-primary'
-					>
-						Confirm password
-					</label>
-					<div className='mt-2'>
-						<input
-							id='re-password'
-							name='re-password'
-							type='password'
-							required
-							className='px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6'
-						/>
-					</div>
-				</div>
-				<div className='flex flex-col gap-2'>
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  // const day: ReactNode[] = [];
+  // const month: ReactNode[] = [];
+  // const year: ReactNode[] = [];
+  // for (let i = 1; i <= 31; i++) {
+  //   day.push(<option value={`${i}`}>{i}</option>);
+  // }
+  // for (let i = 1; i <= 12; i++) {
+  //   month.push(<option value={`${i}`}>{i}</option>);
+  // }
+  // for (let i = 2023; i >= 1990; i--) {
+  //   year.push(<option value={`${i}`}>{i}</option>);
+  // }
+
+  const registrationInputs: RegistrationInput[] = [
+    {
+      label: 'Name',
+      htmlFor: 'name',
+      id: 'name',
+      name: 'name',
+      type: 'name',
+      autocomplete: 'name',
+      required: true,
+      onInput: (e) => {
+        setName(e.target.value);
+      },
+    },
+    {
+      label: 'Email address',
+      htmlFor: 'email',
+      id: 'email',
+      name: 'email',
+      type: 'email',
+      autocomplete: 'email',
+      required: true,
+      onInput: (e) => {
+        setEmail(e.target.value);
+      },
+    },
+    {
+      label: 'Password',
+      htmlFor: 'password',
+      id: 'password',
+      name: 'password',
+      type: 'password',
+      autocomplete: undefined,
+      required: true,
+      onInput: (e) => {
+        setPassword(e.target.value);
+      },
+    },
+    {
+      label: 'Confirm password',
+      htmlFor: 're-password',
+      id: 're-password',
+      name: 're-password',
+      type: 'password',
+      autocomplete: undefined,
+      required: true,
+      onInput: (e) => {
+        setPasswordConfirmation(e.target.value);
+      },
+    },
+  ];
+
+  async function register(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+    let response = await fetch('http://192.168.100.17:8080/api/register', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+        password_confirmation: passwordConfirmation,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+    });
+  }
+
+  return (
+    <div className='w-full flex flex-col justify-center gap-8 items-center py-10 min-h-[70vh] font-poppins'>
+      <div>
+        <h3 className='text-xl lg:text-2xl text-secondary font-semibold'>
+          Create your Free Account
+        </h3>
+      </div>
+      <form
+        className='space-y-6 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4'
+        action='#'
+        onSubmit={(event) => register(event)}
+        method='POST'>
+        {registrationInputs.map((input) => (
+          <div key={input.name}>
+            <label
+              htmlFor={`${input.htmlFor}`}
+              className='block text-sm font-semibold leading-6 text-primary'>
+              {input.label}
+            </label>
+            <div className='mt-2'>
+              <input
+                id={`${input.id}`}
+                name={`${input.name}`}
+                type={`${input.type}`}
+                autoComplete={`${input.autocomplete}`}
+                required={input.required}
+                onInput={input.onInput}
+                className='px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6'
+              />
+            </div>
+          </div>
+        ))}
+        {/* <div className='flex flex-col gap-2'>
 					<label
 						className='block font-semibold text-sm leading-6 text-primary'
 						htmlFor='birthday'
@@ -134,16 +168,16 @@ export default function Registration() {
 							{year}
 						</select>
 					</div>
-				</div>
-				<div>
-					<button
-						type='submit'
-						className='flex w-full justify-center rounded-md bg-secondary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary'
-					>
-						Create an account
-					</button>
-				</div>
-			</form>
-		</div>
-	);
+				</div> */}
+        <div>
+          <button
+            type='submit'
+            key='submit'
+            className='flex w-full justify-center rounded-md bg-secondary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary'>
+            Create an account
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,74 +1,103 @@
+import { ChangeEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SignIn() {
-  const login = fetch('http://192.168.100.17:8080/api/login', {
-    method: 'POST',
-    body: JSON.stringify({
-      email: 'joe2@test.com',
-      password: 'test123',
-    }),
-    headers: {
-      'Content-Type': 'application/json',
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  interface SignInInput {
+    label: string;
+    htmlFor: string;
+    id: string;
+    name: string;
+    type: string;
+    autocomplete: string | undefined;
+    required: boolean;
+    onInput: (value: ChangeEvent<HTMLInputElement>) => void;
+  }
+
+  const signInInputs: SignInInput[] = [
+    {
+      label: 'Email address',
+      htmlFor: 'email',
+      id: 'email',
+      name: 'email',
+      type: 'email',
+      autocomplete: 'email',
+      required: true,
+      onInput: (e) => {
+        setEmail(e.target.value);
+      },
     },
-  });
-  console.log(login);
+    {
+      label: 'Password',
+      htmlFor: 'password',
+      id: 'password',
+      name: 'password',
+      type: 'password',
+      autocomplete: 'password',
+      required: true,
+      onInput: (e) => {
+        setPassword(e.target.value);
+      },
+    },
+  ];
+
+  async function login(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+    let response = await fetch('http://192.168.100.17:8080/api/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+    });
+  }
 
   return (
-    <div className='w-full flex flex-col justify-center gap-8 items-center py-10 min-h-[70vh] font-poppins'>
+    <div className='w-full flex flex-col justify-center items-center py-10 min-h-[70vh] font-poppins'>
       <div className='w-full flex flex-col justify-center gap-4 items-center'>
         <h2 className='text-center text-2xl font-bold leading-9 tracking-tight text-primary'>
           Sign in to your account
         </h2>
       </div>
       <form
-        className='space-y-6 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4'
+        className='mt-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4'
         action='#'
+        onSubmit={(event) => login(event)}
         method='POST'>
-        <div>
-          <label
-            htmlFor='email'
-            className='block text-sm font-medium leading-6 text-primary'>
-            Email address
-          </label>
-          <div className='mt-2'>
-            <input
-              id='email'
-              name='email'
-              type='email'
-              autoComplete='email'
-              required
-              className='px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6'
-            />
-          </div>
-        </div>
-
-        <div>
-          <div className='flex items-center justify-between'>
+        {signInInputs.map((signInInput) => (
+          <div key={signInInput.name} className='mt-6'>
             <label
-              htmlFor='password'
+              htmlFor={`${signInInput.htmlFor}`}
               className='block text-sm font-medium leading-6 text-primary'>
-              Password
+              {signInInput.label}
             </label>
-            <div className='text-sm'>
-              <a
-                href='/forgot'
-                className='font-semibold text-primary hover:text-secondary'>
-                Forgot password?
-              </a>
+            <div className='mt-2'>
+              <input
+                id={`${signInInput.id}`}
+                name={`${signInInput.name}`}
+                type={`${signInInput.type}`}
+                autoComplete={`${signInInput.autocomplete}`}
+                required={signInInput.required}
+                onInput={signInInput.onInput}
+                className='px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6'
+              />
             </div>
           </div>
-          <div className='mt-2'>
-            <input
-              id='password'
-              name='password'
-              type='password'
-              autoComplete='current-password'
-              required
-              className='px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6'
-            />
-          </div>
+        ))}
+        <div className='text-sm text-right mt-2 mb-4'>
+          <a
+            href='/forgot'
+            className='font-semibold text-primary hover:text-secondary'>
+            Forgot password?
+          </a>
         </div>
-
         <div>
           <button
             type='submit'
@@ -76,7 +105,7 @@ export default function SignIn() {
             Sign in
           </button>
         </div>
-        <div className='flex justify-center gap-2 text-base'>
+        <div className='flex justify-center gap-2 text-base mt-4'>
           <p>Not a member? </p>
           <Link
             className='text-secondary hover:text-primary'
